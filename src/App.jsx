56473685a97b6d83f46d2665198c9d4b5f409a66
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-10-27 12:31:40 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="app">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,52 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <aside className={styles.hero}>
+      <div className={styles["hero-bg"]}></div>
+      <div className={styles["hero-overlay"]}>
+        <div className={styles.grids}>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+          <div className={styles.grid}></div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <h1>HELLO</h1>
+      </div>
+    </aside>
+  );
+}
