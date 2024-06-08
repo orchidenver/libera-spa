b9a41a97be5bea66469b08025dce978b7acd71f0
Vsevolod Slavianinov <a0974377355@gmail.com>
2024-06-08 21:50:52 +0300

    Added animation to Token Section

--- src/components/Token/index.jsx
+++ src/components/Token/index.jsx
@@ -1,11 +1,26 @@
+import { useRef, useEffect, useState } from "react";
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 import styles from "./Token.module.css";
 import Token1 from "../../assets/token-1.webp";
 import Token2 from "../../assets/token-2.webp";
 import Tokens from "../../assets/tokens.webp";
 
 export default function Token() {
+  const [visible, setVisible] = useState(false);
+  const ref = useRef();
+  const mobileSize = useMediaQuery("(max-width: 800px)");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setVisible(entry.isIntersecting);
+    });
+
+    observer.observe(ref.current);
+  }, []);
+
   return (
-    <section className={styles.token}>
+    <section className={styles.token} ref={ref}>
       <div className={styles.container}>
         <div className={styles["token__info"]}>
           <h3 className={styles["token__header"]}>
@@ -16,7 +31,11 @@ export default function Token() {
             <img
               src={Token1}
               alt="token-big"
-              className={styles["token__img-big"]}
+              className={
+                visible && mobileSize
+                  ? styles["token__animation"]
+                  : styles["token__img-big"]
+              }
             />
             <img
               src={Token2}
